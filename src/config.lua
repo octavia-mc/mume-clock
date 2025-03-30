9916a68701ff5269f0430ac503a901128891e4a5
Octavia <>
2025-03-30 11:57:08 -0500

    v1.2

---
+++ src/config.lua
@@ -0,0 +1,6 @@
+mpackage = [[mume-clock]]
+author = [[Octavia]]
+title = [[Track and display MUME time and date in Mudlet]]
+description = [[This package tracks and displays the MUME time and date along with other information including dawn/dusk (and time until dawn/dusk), month, season, and which month this is of the current season. This information can be automatically displayed at regular intervals: every MUME hour for time and every MUME day for the date.]]
+version = [[1.2]]
+created = "2025-03-30T10:33:21-05:00"
